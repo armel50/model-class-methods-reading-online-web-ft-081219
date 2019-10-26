31e2a91445ee8145d2973faf0d5ed6baed22473a
Cloe Kouadjo <armelfli50@gmail.com>
2019-10-26 07:07:22 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,6 +14,9 @@ def self.old_news
   where("created_at <?", Time.zone.today.beginning_of_day)
 end
 
+def self.by_author(author_id)
+  where(author: author_id)
+end
   private
 
   def is_title_case
